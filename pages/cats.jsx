07f75f1bc8c958d080e810c825caa8e46d97341d
MauroSerrano <mauro.serrano@hotmail.com.br>
2022-10-20 08:16:00 +0100

    add cat

--- pages/cats.jsx
+++ pages/cats.jsx
@@ -10,6 +10,10 @@ import { BiSearchAlt } from "react-icons/bi"
 export default function Cats() {
   const [cats, setCats] = useState([])
   const [search, setSearch] = useState("")
+  const [editin, setEditin] = useState(false)
+  const [nameImput, setNameImput] = useState("")
+  const [genderImput, setQuantidadeImput] = useState("")
+  const [ageImput, setValidadeImput] = useState("")
 
   function attAllCats() {
     const options = { method: 'GET' };
@@ -19,17 +23,34 @@ export default function Cats() {
       .then(response => setCats(response.cats.filter(cat => cat.name.toLowerCase().includes(search.toLowerCase()))))
       .catch(err => console.error(err));
   }
+
+  function handleAddCat() {
+    setEditin(false)
+    const options = {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify({ name: nameImput, gender: genderImput, age: ageImput })
+    };
+
+    fetch('/api/cats', options)
+      .then(response => response.json())
+      .then(response => console.log(response))
+      .catch(err => console.error(err));
+
+  }
   useEffect(() => {
     attAllCats()
   }, [search]);
 
   return (
     <div className="CatsReact">
-      <div className="contentInputSearchCats">
-        <BiSearchAlt className="iconInputSearchCats" />
-        <input className="inputSearchCats" type={"text"} onChange={(e) => setSearch(e.target.value)} />
+      <div className="topMenuButtonsStock">
+        <button onClick={() => setEditin(true)} className="addButton">Adicionar</button>
+        <div className="contentInputSearchCats">
+          <BiSearchAlt className="iconInputSearchCats" />
+          <input className="inputSearchCats" type={"text"} onChange={(e) => setSearch(e.target.value)} />
+        </div>
       </div>
-
       <h2>Cats</h2>
       <div className="cats">
         {cats.map((cat, i) =>
@@ -46,6 +67,24 @@ export default function Cats() {
             </div>
           </div>)}
       </div>
+      {editin && <div className="addDiv">
+        <div>
+          <p>Name: </p>
+          <input type={"text"} onChange={(e) => setNameImput(e.target.value)} />
+        </div>
+        <div>
+          <p>Gender: </p>
+          <input type={"text"} onChange={(e) => setValidadeImput(e.target.value)} />
+        </div>
+        <div>
+          <p>Age: </p>
+          <input type={"text"} onChange={(e) => setQuantidadeImput(e.target.value)} />
+        </div>
+        <div className="buttonsAddDiv">
+          <button onClick={() => setEditin(false)} >Voltar</button>
+          <button onClick={() => handleAddCat()} >Confirme</button>
+        </div>
+      </div>}
     </div>
   );
 }
