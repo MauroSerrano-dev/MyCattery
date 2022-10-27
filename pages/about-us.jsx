9a8c85fac788b48353cada8c86e39f91ccf016d2
Mauro Serrano <48873552+MauroSerrano-dev@users.noreply.github.com>
2022-10-27 18:34:43 +0100

    Update about-us.jsx

--- pages/about-us.jsx
+++ pages/about-us.jsx
@@ -1,13 +1,1 @@
-import { useState } from "react";
 
-export default function PokerMenu() {
-  return (
-    <div className="aboutUsMain">
-      <p>Created by:</p>
-      <ul>
-        <li>Mauro Serrano</li>
-        <p></p>
-      </ul>
-    </div>
-  );
-}
